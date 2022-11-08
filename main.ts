825abed5ebb67ad79660dfc4c9a4d04684ce2dde
Naifff <knifffe@yandex.ru>
2022-11-08 12:23:23 +0300

    Обновить main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,162 +1,53 @@
-basic.forever(function () {
-    basic.showLeds(`
-        # # . # #
-        . # . # .
-        . # . # .
-        . # . # .
-        # # . # #
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        # . # # .
-        # . # . .
-        # . # . .
-        # . # . .
-        # . # # .
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . # # . .
-        . # . . #
-        . # . . #
-        . # . . #
-        . # # . #
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        # # . . .
-        # . . # #
-        # . . # .
-        # . . # #
-        # # . # .
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        # . . . .
-        . . # # #
-        . . # . #
-        . . # # #
-        # . # . #
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . . . . .
-        . # # # .
-        . # . # .
-        . # # # .
-        . # . # .
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . . . . .
-        # # # . #
-        # . # . #
-        # # # . #
-        # . # . #
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . . . . .
-        # # . # #
-        . # . # .
-        # # . # .
-        . # . # #
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . . . . .
-        # . # # #
-        # . # . .
-        # . # . .
-        # . # # #
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . . . . .
-        . # # # .
-        . # . . .
-        . # . . .
-        . # # # .
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . . . . #
-        # # # . #
-        # . . . #
-        # . . . #
-        # # # . #
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . . . # .
-        # # . # .
-        . . . # .
-        . . . # #
-        # # . # .
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . . # . .
-        # . # . .
-        . . # . #
-        . . # # .
-        # . # . #
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . # . . .
-        . # . . .
-        . # . # .
-        . # # . .
-        . # . # .
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        # . . . .
-        # . . . .
-        # . # . .
-        # # . . .
-        # . # . .
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . # . . .
-        # . . . .
-        . # . . .
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . . . . #
-        . . . . .
-        # . . . .
-        . . . . .
-        # . . . #
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . . . # #
-        . . . . #
-        . . . . #
-        . . . . #
-        . . . # #
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . . # # .
-        . . . # .
-        . . . # .
-        . . . # .
-        . . # # .
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . # # . #
-        . . # . #
-        . . # . #
-        . . # . #
-        . # # . #
-        `)
-    basic.pause(100)
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        images.createBigImage(`
+            # # . # # . . . . .
+            . # . # . . # # # .
+            . # . # . . # . # .
+            . # . # . . # # # .
+            # # . # # . # . # .
+            `).scrollImage(1, 200)
+        images.createBigImage(`
+            . . . . # . . . . .
+            # # # . # . . . . .
+            # . . . # . # . . .
+            # . . . # # . . . .
+            # # # . # . # . . .
+            `).scrollImage(1, 200)
+        images.createBigImage(`
+            . # . . # . . . # #
+            # # # . # . . . # .
+            . # . . # # # . # #
+            . # . . # . # . # .
+            . # # . # . # . # #
+            `).scrollImage(1, 200)
+        images.createBigImage(`
+            # . . . # # # . # .
+            # . . . # . # . # .
+            # . . . # # # . # .
+            . . . . # . . . # .
+            # . . . # . . . # #
+            `).scrollImage(1, 200)
+        images.createBigImage(`
+            . . . . . . . . . .
+            . . # # # . # . . #
+            . . # . # . # # . #
+            . . # # # . # . # #
+            # . # . # . # . . #
+            `).scrollImage(1, 200)
+        images.createBigImage(`
+            . # # # . . # . . #
+            . # . # . # # # . #
+            . # # # . . # . . #
+            . # . . . . # . . .
+            . # # # . . # # . #
+            `).scrollImage(1, 200)
+        images.createBigImage(`
+            . # . # . . . . . .
+            . # . # . . . . . .
+            . # . # . . . . . .
+            . . . . . . . . . .
+            . # . # . . . . . .
+            `).scrollImage(1, 200)
+    }
 })
